9b58aa3ae35451b2f50e7f7a804951684f44180d
José Nuñez <86788881+JosMeeh@users.noreply.github.com>
2024-09-23 01:25:00 -0400

    Update moodal

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import './App.css'
+import React, { useEffect } from 'react';
+import "./App.css";
+import Modal from "./Modal";
+import  Search from "./TableSearchBar";
+
+const App = () => {
+  const handleClick = () => {
+
+  };
+  useEffect(() => {
+
+  }, []);
+
+  return (
+      <div>
+        <Modal />
+         
+      </div>
 
 
-
-//Ejemplo de uso de axio para la autenticacion
-
-const GoogleBooksSearch: React.FC = () => {
-    const [statusCode, setStatusCode] = useState<number | null>(null);
-
-    useEffect(() => {
-        const postData = async () => {
-            try {
-                // Realiza una solicitud POST (esto es solo para el ejemplo)
-                const response = await axios.post('https://localhost:7058/Authentication', {
-                    userName: "tveestjnunez",
-                    password: "Cocacolafemsa.2024#1"
-                });
-                setStatusCode(response.status);
-            } catch (error) {
-                if (axios.isAxiosError(error) && error.response) {
-                    setStatusCode(error.response.status);
-                } else {
-                    setStatusCode(500); // Código de error genérico
-                }
-            }
-        };
-        postData();
-    }, []);
-
-    return (
-        <div>
-            {statusCode !== null ? (
-                <p>Codigo de estado: {statusCode}</p>
-            ) : (
-                <p>Cargando...</p>
-            )}
-        </div>
-    );
+  );
 };
 
-export default GoogleBooksSearch;
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,33 @@
+export const Users= [
+
+    {
+        "Id": "1",
+        "Name": "Juan Miguel",
+        "Type": "Empleado"
+    },
+    {
+        "Id": "2",
+        "Name": "Salvatore",
+        "Type": "Empleado"
+    },{
+        "Id": "3",
+        "Name": "Gianpiero",
+        "Type": "Empleado"
+    },{
+        "Id": "4",
+        "Name": "Carleto",
+        "Type": "Administrador "
+    },{
+        "Id": "5",
+        "Name": "Girono",
+        "Type": "CEO"
+    },{
+        "Id": "6",
+        "Name": "Josoe",
+        "Type": "CEO"
+    },{
+        "Id": "7",
+        "Name": "Juan Miguel",
+        "Type": "CEO"
+    }
+]
